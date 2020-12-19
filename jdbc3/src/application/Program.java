1cf97a5b7ba2b827f8283d0ba4c5d1858baff8a5
csnascimento <cs_nascimento@yahoo.com.br>
2020-12-19 01:45:59 -0300

    236 - Inserir novo funcionario

--- jdbc3/src/application/Program.java
+++ jdbc3/src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -22,9 +24,10 @@ public class Program {
 					"INSERT INTO seller " +
 					"(Name, Email, BirthDate, BaseSalary, DepartmentID) " + 
 					"VALUES " +
-					"(?, ?, ?, ?, ?)"
+					"(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS
 					);
-			st.setString(1, "Carl Purple");	
+			st.setString(1, "Mike Pink");	
 			st.setString(2, "carl@gmail.com");
 			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
 			st.setDouble(4, 3000.0);
@@ -32,7 +35,16 @@ public class Program {
 			
 			int rowsAffected = st.executeUpdate();
 			
-			System.out.println("Done! Rows affected: " + rowsAffected);
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
+			}
+			else {
+				System.out.println("No rown affected!");
+			}
 			
 		}
 		catch (SQLException e) {
