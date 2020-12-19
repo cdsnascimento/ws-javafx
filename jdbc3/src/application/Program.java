b4b6cb3d5b28ae087964ecffd98f97fea3ccf642
csnascimento <cs_nascimento@yahoo.com.br>
2020-12-19 12:59:31 -0300

    236 - inserir dados
    Inserir mais de um departamento e recuperar seus Ids

--- jdbc3/src/application/Program.java
+++ jdbc3/src/application/Program.java
@@ -19,7 +19,7 @@ public class Program {
 		PreparedStatement st = null;
 		try {
 			conn = DB.getConnection();
-			
+			/*
 			st = conn.prepareStatement(
 					"INSERT INTO seller " +
 					"(Name, Email, BirthDate, BaseSalary, DepartmentID) " + 
@@ -27,11 +27,17 @@ public class Program {
 					"(?, ?, ?, ?, ?)",
 					Statement.RETURN_GENERATED_KEYS
 					);
-			st.setString(1, "Mike Pink");	
-			st.setString(2, "carl@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+			st.setString(1, "Joe Yellow");	
+			st.setString(2, "joe@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("21/03/1995").getTime()));
 			st.setDouble(4, 3000.0);
 			st.setInt(5, 4);
+			*/
+			
+			st = conn.prepareStatement(
+					"insert into department (Name) values ('D1'),('D2')",
+					Statement.RETURN_GENERATED_KEYS
+					);
 			
 			int rowsAffected = st.executeUpdate();
 			
@@ -49,10 +55,10 @@ public class Program {
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
-		}
+		}/*
 		catch (ParseException e) {
 			e.printStackTrace();
-		}
+		}*/
 		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
