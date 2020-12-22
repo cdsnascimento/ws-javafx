97f9b19bb5da5b8dac43d9b513871669e3bff2eb
csnascimento <cs_nascimento@yahoo.com.br>
2020-12-22 05:43:42 -0300

    239 -Transacoes

--- jdbc5/src/application/Program.java
+++ jdbc5/src/application/Program.java
@@ -2,9 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+
 import java.sql.SQLException;
-import java.sql.Statement;
+
 
 
 import db.DB;
